49e408a5880f0c6911142250bd0ae087556007b8
nastuy <nastuysandalova@gmail.com>
2025-06-02 06:29:55 +0300

    Update Bow.cpp

--- Bow.cpp
+++ Bow.cpp
@@ -2,11 +2,14 @@
 #include "Arrow.h"
 
 Bow::Bow(float attackSpeed, float damage, float arrowSpeed)
-  : attackSpeed(attackSpeed), damage(damage), arrowSpeed(arrowSpeed) {}
+  : arrowSpeed(arrowSpeed) {
+  this->damage = damage;
+  this->attackSpeed = attackSpeed;
+}
 
 void Bow::attack(const sf::Vector2f& startPos, const sf::Vector2f& targetPos) {
   if (cooldown <= 0.f) {
-    arrows.push_back(std::make_shared<Arrow>(startPos, targetPos, arrowSpeed, damage, getOwner()));
+    arrows.push_back(std::make_shared<Arrow>(startPos, targetPos, arrowSpeed, damage, getOwner(), getOwner()->levelManager));
     cooldown = 30 / attackSpeed;
   }
 }
@@ -14,10 +17,6 @@ void Bow::attack(const sf::Vector2f& startPos, const sf::Vector2f& targetPos) {
 void Bow::update(float deltaTime) {
   cooldown -= deltaTime;
 
-  // Обновляем только живые стрелы
-  
-
-  // Удаляем мертвые стрелы
   arrows.erase(
     std::remove_if(arrows.begin(), arrows.end(),
       [](const auto& arrow) {
